32fd5cb984a64e668bbf7301bbff4d50768a1dad
bahaas <baudoin.haas@gmail.com>
2020-12-03 19:08:20 +0100

    Feat: Redispatch check and select_conversion func
    - Move check_options functions related in a separated file
    - Isolation of select_conversion in a specific file

---
+++ srcs/check_options.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_options.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bahaas <bahaas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 16:22:36 by bahaas            #+#    #+#             */
+/*   Updated: 2020/12/03 18:38:14 by bahaas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+static int check_flags(char *format, t_struct *my_struct)
+{
+	int i;
+
+	i = 1;
+	while (ft_strchr("-0", format[i]))
+	{
+		if (format[i] == '-')
+			my_struct->minus_align = 1;
+		if (format[i] == '0')
+			my_struct->zero_padding = 1;
+		i++;
+	}
+	//printf("\ncheck flags... leftover : %s", &format[i]); //REPERE
+	return (i);
+}
+
+static int check_width(char *format, t_struct *my_struct, va_list args)
+{
+	int i;
+	int pos;
+
+	pos = 0;
+	i = 0;
+	//printf("check width... string start of checking width : %s", &format[pos]); //REPERE
+	while(format[pos] == '0' || format[pos] == '%' || format[pos] == '-')
+		pos++;
+	if (format[pos] == '*')
+	{
+		my_struct->width = va_arg(args, int);
+		return (i + 1);
+	}
+	if (ft_strchr("0123456789", format[pos]))
+		my_struct->width = atoi(&format[pos]);
+	while (ft_strchr("0123456789", format[pos]))
+	{
+		i++;
+		pos++;
+	}
+	//printf("check width... string end of checking width : %s", &format[pos]); //REPERE
+	return (i);
+}
+
+// [WIP] NON FONCTIONNEL -> BESOIN D'UTILISER VAR pos comme pour le check_width
+static int check_precision(char *format, t_struct *my_struct, va_list args)
+{
+	int i;
+
+	i = 0;
+	if (format[i] == '.')
+	{
+		if (format[i + 1] == '*')
+		{
+			my_struct->precision = va_arg(args, int);
+			return (i + 1);
+		}
+		if (ft_strchr("0123456789", format[i]))
+			my_struct->precision = atoi(&format[i]);
+		while(ft_strchr ("0123456789", format[i]))
+			i++;
+	}
+	//printf("check precision... leftover : %s", &format[i]); //REPERE
+	return (i);
+}
+
+int	check_options(t_struct *my_struct, va_list args, char const *format)
+{
+	int i;
+	
+	i = 0;
+	i += check_flags((char *)format, my_struct);			// -0
+	i += check_width((char *)format, my_struct, args);		// *0-9
+	i += check_precision((char *)format, my_struct, args);	// .*0-9
+	//printf("Check struct data :\nzero_padding: %d\nwidth: %d\nprecision: %d\nminus_align: %d\n\n", my_struct->zero_padding, my_struct->width, my_struct->precision, my_struct->minus_align);
+	return(i);
+}
+
---
+++ srcs/select_conversion.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   select_conversion.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bahaas <bahaas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 16:36:24 by bahaas            #+#    #+#             */
+/*   Updated: 2020/12/03 19:02:07 by bahaas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+int	select_conversion(char c, va_list args, t_struct *my_struct)
+{
+	int count;
+
+	count = 0;
+
+	if (c == 'c')
+		count += c_convert(args, my_struct, count);
+	/*
+	else if (c == 's')
+		count += s_convert(args, my_struct);
+	else if (c == 'p')
+		count += p_convert(args, my_struct);
+	else if (c == 'd')
+		count += d_convert(args, my_struct);
+	else if (c == 'i')
+		count += i_convert(args, my_struct);
+	else if (c == 'u')
+		count += u_convert(args, my_struct);
+	else if (c == 'x')
+		count += x_convert(args, my_struct);
+	else if (c == 'X')
+		count += X_convert(args, my_struct);
+	else if (c == '%')
+		count += percent_convert(args, my_struct);
+	*/
+	return (count);
+}
+
