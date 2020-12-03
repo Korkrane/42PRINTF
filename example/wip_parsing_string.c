98e997461f04f0853462d5a4e4f74627ac1f638e
bahaas <baudoin.haas@gmail.com>
2020-12-03 15:39:10 +0100

    Add:
    - Many examples added
    - Start of args parsing

--- example/wip_parsing_string.c
+++ example/wip_parsing_string.c
@@ -6,13 +6,14 @@
 /*   By: bahaas <bahaas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 16:54:40 by bahaas            #+#    #+#             */
-/*   Updated: 2020/11/27 16:48:03 by bahaas           ###   ########.fr       */
+/*   Updated: 2020/12/02 19:37:03 by bahaas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -35,38 +36,43 @@ char	*parse(char *s)
 	int		i;
 	char	*set;
 
-	set = "0123456789+-.*";
+	set = "cspduixX%0123456789-.*";
 	i = 0;
-	while (s[i])
+	while (s[i] != '\0')
 	{
 		if (s[i] != '%')
-		{
 			printf("%c", s[i]);
-			i++;
-		}
-		else if (s[i] == '%')
+		else if(s[i] == '%')
 		{
+			if(!ft_strchr(set, s[i + 1]))
+				break ;
 			printf("1");
 			while (ft_strchr(set, s[i + 1]))
 			{
-				printf("0");
 				i++;
-				if (ft_strchr("cspdiuxX%", s[i + 1]))
+				if(ft_strchr("0123456789.-*", s[i]))
 				{
-					printf("1");
+					printf("0");
 					i++;
+				}
+				if (ft_strchr("cspdiuxX%", s[i]))
+				{
+					i++;
+					printf("1");
 					break ;
 				}
 			}
-			i++;
+			continue;
 		}
+		i++;
 	}
 	return (s);
 }
 
-char	*ft_printf(char *s)
+char	*ft_test(char *s)
 {
-	return (parse(s));
+	parse(s);
+	return (s);
 }
 
 int		main(void)
@@ -75,8 +81,11 @@ int		main(void)
 	int		i;
 
 	i = 4;
-	result = ft_printf("Print with s : %+.5d and %s");
-	//printf("%s\n", result);
-	printf("\n%+.5d\n", i);
+	printf("My parsing :\n");
+	result = ft_test("Hello world ! Put some args %-.5d and %s");
+	printf("\nlength : %lu\n", strlen(result));
+	printf("\nParsed string :\n");
+	printf("Hello world ! Put some args %%-.5d and %%s");
+	printf("\nlength : %lu\n", strlen("Hello world ! Put some args %%-.5d and %%s")); //diff de 2 a cause des %%
 	return (0);
 }
